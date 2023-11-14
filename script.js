6e33ab81c4e7d6bcca10678ff907b6cf44a53cb9
Nilaxsh <nilaxsakana@gmail.com>
2023-11-14 18:01:51 +0530

    assignment

---
+++ script.js
@@ -0,0 +1,54 @@
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+   //gallery
+  const filterContainer = document.querySelector(".gallery-filter"),
+  galleryItems = document.querySelectorAll(".gallery-item");
+
+ filterContainer.addEventListener("click", (event) =>{
+   if(event.target.classList.contains("filter-item")){
+   	 // deactivate existing active 'filter-item'
+   	 filterContainer.querySelector(".active").classList.remove("active");
+   	 // activate new 'filter-item'
+   	 event.target.classList.add("active");
+   	 const filterValue = event.target.getAttribute("data-filter");
+   	 galleryItems.forEach((item) =>{
+       if(item.classList.contains(filterValue) || filterValue === 'all'){
+       	item.classList.remove("hide");
+       	 item.classList.add("show");
+       }
+       else{
+       	item.classList.remove("show");
+       	item.classList.add("hide");
+       }
+   	 });
+   }
+ });
+  
+   
+  //freequent section
+  const faqHeaders = document.querySelectorAll(".faqs-container .faq-header");
+
+faqHeaders.forEach((header, i) => {
+  header.addEventListener("click", () => {
+    header.nextElementSibling.classList.toggle("active");
+
+    const open = header.querySelector(".open");
+    const close = header.querySelector(".close");
+
+    if (header.nextElementSibling.classList.contains("active")) {
+      open.classList.remove("active");
+      close.classList.add("active");
+    } else {
+      open.classList.add("active");
+      close.classList.remove("active");
+    }
+  });
+});
+
+
+
+
+  
